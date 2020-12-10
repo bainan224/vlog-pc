6019ed96f860ff10d582c3e1b866fbebbcaf79a3
bainan224 <2403906364@qq.com>
2020-12-10 19:31:06 +0800

    登录功能

---
+++ vlog-pc/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app id="app">
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+<style lang="scss">
+.move {
+  animation-duration: 0.6s;
+  animation-delay: 0.2s;
+}
+</style>
---
+++ vlog-pc/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import vuetify from '@/plugins/vuetify'
+axios.defaults.baseURL = 'http://vlog1.cn.utools.club/api'
+
+Vue.use(VueAxios, axios)
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App)
+}).$mount('#app')
